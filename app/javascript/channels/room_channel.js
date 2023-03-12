e35a31a4aa6c29cc2470998c02ba0c165dbd16e1
UditThakkar29 <uthakkar@gammastack.com>
2023-03-12 10:35:35 +0000

    Basic Action Cable Done

---
+++ app/javascript/channels/room_channel.js
@@ -0,0 +1,35 @@
+import consumer from "./consumer"
+
+consumer.subscriptions.create("RoomChannel", {
+  connected() {
+    console.log("We are connected")
+    // Called when the subscription is ready for use on the server
+  },
+
+  disconnected() {
+    // Called when the subscription has been terminated by the server
+  },
+
+  received(data) {
+    // Called when there's incoming data on the websocket for this channel
+    $('#msg').append('<div class="message"> ' + data.content + '</div>')
+    console.log(data.content)
+  }
+});
+
+var submit_messages;
+
+$(document).on('turbolinks:load', function() {
+  submit_messages()
+})
+
+submit_messages = function(){
+  $('#message_content').on('keydown',function(event){
+    if(event.keyCode === 13 ){
+      $('input').click()
+      event.target.value = ""
+      event.preventDefault() // prevent user from hiting enter when the content is emplty
+      console.log("yes we hitted enter")
+    }
+  })
+}
